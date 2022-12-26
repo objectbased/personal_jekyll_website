ee0ff6bf805cd524a11dba382d456ba42a00980c
objectbased <67526309+objectbased@users.noreply.github.com>
2022-12-26 15:36:17 -0500

    c
    c

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,147 +1,43 @@
-const togglePython = document.querySelector('#skill_python')
-const PythonProjectList = document.querySelectorAll(`[id^="splunk"], [id^="ansible"], [id^="aws"], [id^="bash"], [id^="terraform"], [id^="github"]`)
-var PythonProjectListLength = PythonProjectList.length;
+filterSelection("all")
 
-const toggleBash = document.querySelector('#skill_bash')
-const BashProjectList = document.querySelectorAll(`[id^="splunk"], [id^="ansible"], [id^="aws"], [id^="python"], [id^="terraform"], [id^="github"]`)
-var BashProjectListLength = BashProjectList.length;
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("card");
+  if (c == "all") c = "";
+  for (i = 0; i < x.length; i++) {
+    w3RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+  }
+}
 
-const toggleAnsible = document.querySelector('#skill_ansible')
-const AnsibleProjectList = document.querySelectorAll(`[id^="splunk"], [id^="python"], [id^="aws"], [id^="bash"], [id^="terraform"], [id^="github"]`)
-var AnsibleProjectListLength = AnsibleProjectList.length;
+function w3AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
+  }
+}
 
-const toggleAws = document.querySelector('#skill_aws')
-const AwsProjectList = document.querySelectorAll(`[id^="splunk"], [id^="ansible"], [id^="python"], [id^="bash"], [id^="terraform"], [id^="github"]`)
-var AwsProjectListLength = AwsProjectList.length;
+function w3RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);     
+    }
+  }
+  element.className = arr1.join(" ");
+}
 
-const toggleGithub = document.querySelector('#skill_github')
-const GithubProjectList = document.querySelectorAll(`[id^="splunk"], [id^="ansible"], [id^="aws"], [id^="bash"], [id^="terraform"], [id^="python"]`)
-var GithubProjectListLength = GithubProjectList.length;
-
-const toggleTerraform = document.querySelector('#skill_terraform')
-const TerraformProjectList = document.querySelectorAll(`[id^="splunk"], [id^="ansible"], [id^="aws"], [id^="bash"], [id^="python"], [id^="github"]`)
-var TerraformProjectListLength = TerraformProjectList.length;
-
-const toggleSplunk = document.querySelector('#skill_splunktest')
-const SplunkProjectList = document.querySelectorAll(`[id^="python"], [id^="ansible"], [id^="aws"], [id^="bash"], [id^="terraform"], [id^="github"]`)
-var SplunkProjectListLength = SplunkProjectList.length;
-
-
-// Python
-togglePython.addEventListener('click', () => {
-  if(event.target.style.backgroundColor == 'blue') {
-    event.target.style.backgroundColor = 'black';
-  } else {
-    event.target.style.backgroundColor = 'blue';
-  }
-  for (var i = 0; i < PythonProjectListLength; i++) {
-    console.log(PythonProjectList[i]);
-    if(PythonProjectList[i].style.display == 'none') {
-      PythonProjectList[i].style.display = 'block';
-    } else {
-      PythonProjectList[i].style.display = 'none'
-  }
-  }
-});
-
-// Bash
-toggleBash.addEventListener('click', () => {
-  if(event.target.style.backgroundColor == 'blue') {
-    event.target.style.backgroundColor = 'black';
-  } else {
-    event.target.style.backgroundColor = 'blue';
-  }
-  for (var i = 0; i < BashProjectListLength; i++) {
-    console.log(BashProjectList[i]);
-    if(BashProjectList[i].style.display == 'none') {
-      BashProjectList[i].style.display = 'block';
-    } else {
-      BashProjectList[i].style.display = 'none'
-  }
-  }
-});
-
-// Ansible
-toggleAnsible.addEventListener('click', () => {
-  if(event.target.style.backgroundColor == 'blue') {
-    event.target.style.backgroundColor = 'black';
-  } else {
-    event.target.style.backgroundColor = 'blue';
-  }
-  for (var i = 0; i < AnsibleProjectListLength; i++) {
-    console.log(AnsibleProjectList[i]);
-    if(AnsibleProjectList[i].style.display == 'none') {
-      AnsibleProjectList[i].style.display = 'block';
-    } else {
-      AnsibleProjectList[i].style.display = 'none'
-  }
-  }
-});
-
-// AWS
-toggleAws.addEventListener('click', () => {
-  if(event.target.style.backgroundColor == 'blue') {
-    event.target.style.backgroundColor = 'black';
-  } else {
-    event.target.style.backgroundColor = 'blue';
-  }
-  for (var i = 0; i < AwsProjectListLength; i++) {
-    console.log(AwsProjectList[i]);
-    if(AwsProjectList[i].style.display == 'none') {
-      AwsProjectList[i].style.display = 'block';
-    } else {
-      AwsProjectList[i].style.display = 'none'
-  }
-  }
-});
-
-// GitHub
-toggleGithub.addEventListener('click', () => {
-  if(event.target.style.backgroundColor == 'blue') {
-    event.target.style.backgroundColor = 'black';
-  } else {
-    event.target.style.backgroundColor = 'blue';
-  }
-  for (var i = 0; i < GitHubProjectListLength; i++) {
-    console.log(GithubProjectList[i]);
-    if(GithubProjectList[i].style.display == 'none') {
-      GithubProjectList[i].style.display = 'block';
-    } else {
-      GithubProjectList[i].style.display = 'none'
-  }
-  }
-});
-
-// Terraform
-toggleTerraform.addEventListener('click', () => {
-  if(event.target.style.backgroundColor == 'blue') {
-    event.target.style.backgroundColor = 'black';
-  } else {
-    event.target.style.backgroundColor = 'blue';
-  }
-  for (var i = 0; i < TerraformProjectListLength; i++) {
-    console.log(TerraformProjectList[i]);
-    if(TerraformProjectList[i].style.display == 'none') {
-      TerraformProjectList[i].style.display = 'block';
-    } else {
-      TerraformProjectList[i].style.display = 'none'
-  }
-  }
-});
-
-// Splunk
-toggleSplunk.addEventListener('click', () => {
-  if(event.target.style.backgroundColor == 'blue') {
-    event.target.style.backgroundColor = 'black';
-  } else {
-    event.target.style.backgroundColor = 'blue';
-  }
-  for (var i = 0; i < SplunkProjectListLength; i++) {
-    console.log(SplunkProjectList[i]);
-    if(SplunkProjectList[i].style.display == 'none') {
-      SplunkProjectList[i].style.display = 'block';
-    } else {
-      SplunkProjectList[i].style.display = 'none'
-  }
-  }
-});
+// Add active class to the current button (highlight it)
+var btnContainer = document.getElementsByClassName("skills");
+var btns = btnContainer.getElementsByClassName("button-filter");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function(){
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
